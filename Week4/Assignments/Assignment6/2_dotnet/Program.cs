b214a7eafad63538bd20c0d4d5befadbb9d26623
Krishanu Mishra <krishanu.mishra@intimetec.com>
2025-04-15 09:48:14 +0530

    Week4 Assignments done

---
+++ Week4/Assignments/Assignment6/2_dotnet/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotnetDockerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Dockerized .NET Core App!");
+        }
+    }
+}
